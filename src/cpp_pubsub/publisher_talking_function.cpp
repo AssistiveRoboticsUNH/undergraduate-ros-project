4dfa7a9b0529943f5ad2752c4273977d5b9a56f1
cgladu <cgladu92@gmail.com>
2022-11-01 14:58:20 -0400

    Made some stuff in CMake so it builds

--- src/cpp_pubsub/publisher_talking_function.cpp
+++ src/cpp_pubsub/publisher_talking_function.cpp
@@ -17,9 +17,9 @@ public:
     MinimalPublisher()
             : Node("minimal_publisher"), count_(0)
     {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-        timer_ = this->create_wall_timer(
-                10000ms, std::bind(&MinimalPublisher::timer_callback, this));
+        publisher_ = this->create_publisher<std_msgs::msg::String>("/speech", 10);
+        timer_callback();
+
     }
 
 private:
