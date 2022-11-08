2ef2a2fdb89d76256764282eb57a3f96689b02f5
cgladu <cgladu92@gmail.com>
2022-11-08 15:32:52 -0500

    Nao should be able to listen and make a wav file output of what here hears

--- src/py_pubsub/py_pubsub/talkerNode.py
+++ src/py_pubsub/py_pubsub/talkerNode.py
@@ -1,23 +1,53 @@
+from __future__ import print_function
+import scipy.io.wavfile as wavf
+import numpy as np
+
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 from naoqi_bridge_msgs.msg import AudioBuffer
+from naoqi_bridge_msgs.msg import HeadTouch
+
 
 class TalkerNode(Node):
+
 
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, '/speech', 10)
-        self.subscription = self.create_subscription(
-            AudioBuffer,
-            '/audio',
-            self.listener_callback,
-            10)
+        self.subscription = self.create_subscription(AudioBuffer, '/audio', self.listener_callback, 10)
+        self.subscription = self.create_subscription(HeadTouch, '/headtouch', self.headtouch_callback, 10)
+        self.data = []
+        self.listening = False
+
+
+    def headgtouch_callback(self, msg):
+        self.get_logger().info('Head touched')
+
+        if self.listening:
+            self.listening = False
+            temp = String()
+            temp.data = 'I am done listening now.'
+            self.publisher.publish(temp)
+            self.interpret()
+
+        else:
+            self.listening = True
+            temp = String()
+            temp.data = 'I am listening now.'
+            self.publisher.publish(temp)
 
     def listener_callback(self, msg):
-        self.get_logger().info('Info recieved')
 
+        if self.listening:
+            self.data.append(msg.data);
+            self.get_logger().info('Info recieved')
+
+    def interpret(self):
+        frequency = 48000
+        out_f = 'out.wav'
+        wavf.write(out_f, frequency, np.array(self.data))
 
 def main(args=None):
     rclpy.init(args=args)
