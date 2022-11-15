791f820f44b148884f57dcb4687ac53552875199
cgladu <cgladu92@gmail.com>
2022-11-15 14:10:38 -0500

    Spin with action server

--- src/rotate/setup.py
+++ src/rotate/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rotate_action = rotate.rotateNode:main',
+            'rotate_action = rotate.rotate_action:main',
         ],
     },
 )
