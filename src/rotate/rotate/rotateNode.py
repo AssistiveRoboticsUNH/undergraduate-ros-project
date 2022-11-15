f8c007401fb6aac55859030c19c72e882d0acde4
cgladu <cgladu92@gmail.com>
2022-11-15 13:21:59 -0500

    Move where it finds the time

--- src/rotate/rotate/rotateNode.py
+++ src/rotate/rotate/rotateNode.py
@@ -21,9 +21,7 @@ class RotateNode(Node):
         self.first = 0
 
     def spin_callback(self, msg):
-        now = datetime.now()
         self.first = 1
-        self.lastTouched = now
         temp = Twist()
         temp.angular.z = 0.2
         self.publisher.publish(temp)
@@ -31,13 +29,14 @@ class RotateNode(Node):
     def odom_callback(self, msg):
         # self.get_logger().info("odom recieved")
         current_orientation = 2 * math.acos(msg.pose.pose.orientation.w)
+        now = datetime.now()
         if self.first == 1:
             self.initial_orientation = current_orientation
+            self.lastTouched = now
             self.get_logger().info("Starting " + str(self.initial_orientation))
             self.get_logger().info("Starting " + str(self.lastTouched))
             self.first = 0
         else:
-            now = datetime.now()
             difference = abs(self.initial_orientation) - abs(current_orientation)
             if abs(difference) < .1 and self.lastTouched + timedelta(seconds=3) < now:
                 self.get_logger().info("Time at stop: " + str(now))
